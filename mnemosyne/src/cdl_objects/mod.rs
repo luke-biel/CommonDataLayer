c67872f08f0602efb2c723a9056569b2293355b5
Łukasz Biel <lukasz.p.biel@gmail.com>
2021-01-12 14:10:35 +0100

    Add notice

--- mnemosyne/src/cdl_objects/mod.rs
+++ mnemosyne/src/cdl_objects/mod.rs
@@ -3,3 +3,5 @@ pub mod all_schemas;
 pub mod schema_preview;
 pub mod update_query_address;
 pub mod update_topic;
+
+// TODO: each of above types can return { data: null, error: {..} } - handle this
