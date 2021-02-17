4539eb6c5636d6dbf0550b5ecef67eb6a551a296
Sam Mohr <sam@mohr.codes>
2021-02-17 02:42:33 -0500

    consolidate types

--- crates/schema-registry/src/types.rs
+++ crates/schema-registry/src/types.rs
@@ -1,60 +1,54 @@
-use indradb::{EdgeProperties, VertexProperties};
-use rpc::schema_registry::types::SchemaType;
 use semver::{Version, VersionReq};
-use serde::{de::DeserializeOwned, Deserialize, Serialize};
+use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use std::collections::HashMap;
 use uuid::Uuid;
 
-use storage::*;
-
-pub mod storage {
-    pub mod edges;
-    pub mod vertices;
-}
-
 // Helper structures
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct ViewUpdate {
-    pub name: Option<String>,
-    pub jmespath: Option<String>,
+pub struct Schema {
+    pub id: Uuid,
+    pub name: String,
+    pub queue: String,
+    pub query_addr: String,
+    pub r#type: SchemaType,
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct NewSchema {
     pub name: String,
     pub definition: Value,
-    pub kafka_topic: String,
-    pub query_address: String,
-    pub schema_type: SchemaType,
-}
-
-impl NewSchema {
-    pub fn vertex(self) -> (vertices::Schema, Value) {
-        let Self {
-            name,
-            definition,
-            kafka_topic,
-            query_address,
-            schema_type,
-        } = self;
-        (
-            vertices::Schema {
-                name,
-                kafka_topic,
-                query_address,
-                schema_type,
-            },
-            definition,
-        )
-    }
+    pub queue: String,
+    pub query_addr: String,
+    pub r#type: SchemaType,
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct NewSchemaVersion {
-    pub version: Version,
-    pub definition: Value,
+pub struct SchemaUpdate {
+    pub name: Option<String>,
+    pub queue: Option<String>,
+    pub query_addr: Option<String>,
+    pub r#type: Option<SchemaType>,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct SchemaWithDefinitions {
+    pub id: Uuid,
+    pub name: String,
+    pub queue: String,
+    pub query_addr: String,
+    pub r#type: SchemaType,
+    pub definitions: Vec<SchemaDefinition>,
+}
+
+impl SchemaWithDefinitions {
+    pub fn definition(&self, version: VersionReq) -> Option<&SchemaDefinition> {
+        self.definitions
+            .iter()
+            .filter(|d| version.matches(&d.version))
+            .max_by_key(|d| d.version)
+    }
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
@@ -89,34 +83,42 @@ impl VersionedUuid {
     }
 }
 
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, sqlx::Type)]
+#[sqlx(type_name = "schema_type", rename_all = "lowercase")]
+pub enum SchemaType {
+    DocumentStorage,
+    Timeseries,
+}
+
+impl Default for SchemaType {
+    fn default() -> Self {
+        SchemaType::DocumentStorage
+    }
+}
+
+impl std::fmt::Display for SchemaType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(match self {
+            SchemaType::DocumentStorage => "DocumentStorage",
+            SchemaType::Timeseries => "Timeseries",
+        })
+    }
+}
+
+impl std::str::FromStr for SchemaType {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "DocumentStorage" => Ok(SchemaType::DocumentStorage),
+            "Timeseries" => Ok(SchemaType::Timeseries),
+            invalid => Err(anyhow::anyhow!("Invalid schema type: {}", invalid)),
+        }
+    }
+}
+
 // Import export
-#[derive(Default, Clone, Debug, Serialize, Deserialize, PartialEq)]
+#[derive(Default, Clone, Debug, Serialize, Deserialize)]
 pub struct DbExport {
-    pub schemas: HashMap<Uuid, vertices::Schema>,
-    pub definitions: HashMap<Uuid, vertices::Definition>,
-    pub views: HashMap<Uuid, vertices::View>,
-    pub schema_definitions: Vec<edges::SchemaDefinition>,
-    pub schema_views: Vec<edges::SchemaView>,
-}
-
-fn extract_vertex_property<T: DeserializeOwned>(
-    properties: &mut VertexProperties,
-    name: &'static str,
-) -> Option<T> {
-    properties
-        .props
-        .drain_filter(|prop| prop.name == name)
-        .next()
-        .and_then(|prop| serde_json::from_value(prop.value).ok())
-}
-
-fn extract_edge_property<T: DeserializeOwned>(
-    properties: &mut EdgeProperties,
-    name: &'static str,
-) -> Option<T> {
-    properties
-        .props
-        .drain_filter(|prop| prop.name == name)
-        .next()
-        .and_then(|prop| serde_json::from_value(prop.value).ok())
+    pub schemas: HashMap<Uuid, SchemaWithDefinitions>,
 }
