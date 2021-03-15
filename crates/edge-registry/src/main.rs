b73e5293e7872127f2f67094503fc83f5f2f47e2
Łukasz Biel <lukasz.p.biel@gmail.com>
2021-03-15 13:15:53 +0100

    Fix after merge

--- crates/edge-registry/src/main.rs
+++ crates/edge-registry/src/main.rs
@@ -6,9 +6,7 @@ use tonic::transport::Server;
 #[tokio::main]
 async fn main() {
     let config = RegistryConfig::from_args();
-    let registry = EdgeRegistryImpl::new(&config)
-        .await
-        .unwrap();
+    let registry = EdgeRegistryImpl::new(&config).await.unwrap();
     Server::builder()
         .add_service(EdgeRegistryServer::new(registry))
         .serve(([0, 0, 0, 0], config.communication_port).into())
--- crates/rpc/build.rs
+++ crates/rpc/build.rs
@@ -1,7 +1,6 @@
 fn main() -> std::io::Result<()> {
     tonic_build::configure().compile(
         &[
-            "proto/command_service.proto",
             "proto/query_service.proto",
             "proto/query_service_ts.proto",
             "proto/schema_registry.proto",
